357f67f6feccee38cd6c324face0fb58c8c0ba07
github username <github email address>
2015-09-09 14:48:41 -0700

    Use SSL and the Puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  .
-  .
-  .
+  
+  
+  
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
